013c1f59dc7ff75032b174c7a6e7a8112792214b
Jeb <ja7ad.borhani@gmail.com>
2020-12-12 21:23:22 +0330

    Port from netcoreapp2.2 to net5.0

--- TcpEchoServer/Build/RunClient.bat
+++ TcpEchoServer/Build/RunClient.bat
@@ -1,4 +1,4 @@
 echo start
 sleep 2
-start "Clients" cmd.exe /k "dotnet EchoClient\Release\netcoreapp2.2\EchoClient.dll -c 1000 -p 1 -s 127.0.0.1:50010 127.0.0.1:50011 127.0.0.1:50012 127.0.0.1:50013 127.0.0.1:50014"
+start "Clients" cmd.exe /k "dotnet EchoClient\Release\net5.0\EchoClient.dll -c 1000 -p 1 -s 127.0.0.1:50010 127.0.0.1:50011 127.0.0.1:50012 127.0.0.1:50013 127.0.0.1:50014"
 exit
--- TcpEchoServer/Build/RunServers.bat
+++ TcpEchoServer/Build/RunServers.bat
@@ -1,12 +1,11 @@
 echo start
 REM Start Backbone
-start "Backbone" cmd.exe /k "dotnet Backbone\Release\netcoreapp2.2\Backbone.dll -b 0.0.0.0:50001 "
+start "Backbone" cmd.exe /k "dotnet Backbone\Release\net5.0\Backbone.dll -b 0.0.0.0:50001 "
 sleep 2
 REM Start Servers
-start "Server 1" cmd.exe /k "dotnet EchoServer\Release\netcoreapp2.2\EchoServer.dll -s 0.0.0.0:50010 -b 127.0.0.1:50001 "
-start "Server 2" cmd.exe /k "dotnet EchoServer\Release\netcoreapp2.2\EchoServer.dll -s 0.0.0.0:50011 -b 127.0.0.1:50001 "
-start "Server 3" cmd.exe /k "dotnet EchoServer\Release\netcoreapp2.2\EchoServer.dll -s 0.0.0.0:50012 -b 127.0.0.1:50001 "
-start "Server 4" cmd.exe /k "dotnet EchoServer\Release\netcoreapp2.2\EchoServer.dll -s 0.0.0.0:50013 -b 127.0.0.1:50001 "
-start "Server 5" cmd.exe /k "dotnet EchoServer\Release\netcoreapp2.2\EchoServer.dll -s 0.0.0.0:50014 -b 127.0.0.1:50001 "
-
+start "Server 1" cmd.exe /k "dotnet EchoServer\Release\net5.0\EchoServer.dll -s 0.0.0.0:50010 -b 127.0.0.1:50001 "
+start "Server 2" cmd.exe /k "dotnet EchoServer\Release\net5.0\EchoServer.dll -s 0.0.0.0:50011 -b 127.0.0.1:50001 "
+start "Server 3" cmd.exe /k "dotnet EchoServer\Release\net5.0\EchoServer.dll -s 0.0.0.0:50012 -b 127.0.0.1:50001 "
+start "Server 4" cmd.exe /k "dotnet EchoServer\Release\net5.0\EchoServer.dll -s 0.0.0.0:50013 -b 127.0.0.1:50001 "
+start "Server 5" cmd.exe /k "dotnet EchoServer\Release\net5.0\EchoServer.dll -s 0.0.0.0:50014 -b 127.0.0.1:50001 "
 exit
